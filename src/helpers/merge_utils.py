58a13ae9c21899a3d84237c59da38e861e344e96
ccthatsme <cciric44@gmail.com>
2026-05-28 12:04:43 -0400

    small update to helper

--- src/helpers/merge_utils.py
+++ src/helpers/merge_utils.py
@@ -13,6 +13,6 @@ DF_RAS_MERGE_KEYS = ['name_for_merge', 'Draft Team', 'Round', 'Year']
 # df_merged_draft_base_left = pd.merge(df_draft, df_ras, left_on=['name_for_merge', 'team', 'draft_round', 'year'], right_on=['name_for_merge', 'Draft Team', 'Round', 'Year'], how='left', validate="m:1")
 # df_merged_ras_base_right = pd.merge(df_draft, df_ras, left_on=['name_for_merge', 'team', 'draft_round', 'year'], right_on=['name_for_merge', 'Draft Team', 'Round', 'Year'], how='right', validate="m:1")
 
-def merge_datasets(left_df, right_df, left_merge_keys, right_merge_keys, how, validate = None):
+def merge_datasets(left_df, right_df, left_merge_keys, right_merge_keys, how, validate = None, indicator = False):
     """Merge two datasets"""    
-    return pd.merge(left_df, right_df, left_on=left_merge_keys, right_on=right_merge_keys, how=how, validate=validate)
+    return pd.merge(left_df, right_df, left_on=left_merge_keys, right_on=right_merge_keys, how=how, validate=validate, indicator=indicator)
